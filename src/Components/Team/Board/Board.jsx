1a574763307c9cb6763ffcaebdc9a9b589f9b23c
Yahia-153 <yahiametwally153@gmail.com>
2024-09-07 18:41:42 +0300

    Team Page

--- src/Components/Team/Board/Board.jsx
+++ src/Components/Team/Board/Board.jsx
@@ -1,9 +1,93 @@
-import './Board.module.css'
+import '../style.module.css'
+import style from '../style.module.css';
+import FB from '../icons/Facebook.png'
+import IG from '../icons/Instagram.png'
+import LI from '../icons/Linkedin.png'
 
 const Board = () => {
   return (
-    <div>Board</div>
+    <>
+      <section class={style.team}>
+        <h1>Shbab Btsad Shbab <span>Board</span></h1>
+        <div class={style.cards} id="Board">
+          <div class={style.card}>
+            <div class={style.girl}>
+            <img src="" alt="" />
+          </div>
+          <p class={style.title}>CEO</p>
+          <h3 class={style.name}>Shahd Nooh</h3>
+          <div class={style.social}>
+            <a href=""><img src={FB} alt="" /></a>
+            <a href=""><img src={IG} alt="" /></a>
+            <a href=""><img src={LI} alt="" /></a>
+          </div>
+        </div>
+          <div class={style.card}>
+            <div class={style.boy}>
+            <img src="" alt="" />
+          </div>
+          <p class={style.title}>Chairman</p>
+          <h3 class={style.name}>Mohsen El Sayed</h3>
+          <div class={style.social}>
+            <a href=""><img src={FB} alt="" /></a>
+            <a href=""><img src={IG} alt="" /></a>
+            <a href=""><img src={LI} alt="" /></a>
+          </div>
+        </div>
+          <div class={style.card}>
+            <div class={style.girl}>
+            <img src="" alt="" />
+          </div>
+          <p class={style.title}>V.CEO</p>
+          <h3 class={style.name}>Sama Nooh</h3>
+          <div class={style.social}>
+            <a href=""><img src={FB} alt="" /></a>
+            <a href=""><img src={IG} alt="" /></a>
+            <a href=""><img src={LI} alt="" /></a>
+          </div>
+        </div>
+          <div class={style.card}>
+            <div class={style.girl}>
+            <img src="" alt="" />
+          </div>
+          <p class={style.title}>COO</p>
+          <h3 class={style.name}>Radwa El Gendy</h3>
+          <div class={style.social}>
+            <a href=""><img src={FB} alt="" /></a>
+            <a href=""><img src={IG} alt="" /></a>
+            <a href=""><img src={LI} alt="" /></a>
+          </div>
+        </div>
+          <div class={style.card}>
+            <div class={style.girl}>
+            <img src="" alt="" />
+          </div>
+          <p class={style.title}>CCO</p>
+          <h3 class={style.name}>Salma Ahmed</h3>
+          <div class={style.social}>
+            <a href=""><img src={FB} alt="" /></a>
+            <a href=""><img src={IG} alt="" /></a>
+            <a href=""><img src={LI} alt="" /></a>
+          </div>
+        </div>
+          <div class={style.card}>
+            <div class={style.girl}>
+            <img src="" alt="" />
+          </div>
+          <p class={style.title}>CAO</p>
+          <h3 class={style.name}>Nour Ahmed</h3>
+          <div class={style.social}>
+            <a href=""><img src={FB} alt="" /></a>
+            <a href=""><img src={IG} alt="" /></a>
+            <a href=""><img src={LI} alt="" /></a>
+          </div>
+        </div>
+      </div>
+    </section >
+    </>
+    
   )
+  
 }
 
 export default Board
--- src/Components/Team/Leaders/Leaders.jsx
+++ src/Components/Team/Leaders/Leaders.jsx
@@ -1,8 +1,102 @@
-import './Leaders.module.css'
+import '../style.module.css'
+import style from '../style.module.css';
+import FB from '../icons/Facebook.png'
+import IG from '../icons/Instagram.png'
+import LI from '../icons/Linkedin.png'
 
 const Leaders = () => {
   return (
-    <div>Leaders</div>
+    <>
+      <section class={style.team}>
+        <h1>Shbab Btsad Shbab <span>Leaders</span></h1>
+        <div class={style.cards} id="leaders">
+          <div class={style.card}>
+            <div class={style.boy}>
+              <img src="" alt="" />
+            </div>
+            <p class={style.title}>Head Of HR</p>
+            <h3 class={style.name}>Omar Mohamed</h3>
+            <div class={style.social}>
+              <a href=""><img src={FB} alt="" /></a>
+              <a href=""><img src={IG} alt="" /></a>
+              <a href=""><img src={LI} alt="" /></a>
+            </div>
+          </div>
+          <div class={style.card}>
+            <div class={style.girl}>
+              <img src="" alt="" />
+            </div>
+            <p class={style.title}>Head Of Marketing</p>
+            <h3 class={style.name}>Sara Eid</h3>
+            <div class={style.social}>
+              <a href=""><img src={FB} alt="" /></a>
+              <a href=""><img src={IG} alt="" /></a>
+              <a href=""><img src={LI} alt="" /></a>
+            </div>
+          </div>
+          <div class={style.card}>
+            <div class={style.boy}>
+              <img src="" alt="" />
+            </div>
+            <p class={style.title}>Head Of Content</p>
+            <h3 class={style.name}>Mohamed Tamer</h3>
+            <div class={style.social}>
+              <a href=""><img src={FB} alt="" /></a>
+              <a href=""><img src={IG} alt="" /></a>
+              <a href=""><img src={LI} alt="" /></a>
+            </div>
+          </div>
+          <div class={style.card}>
+            <div class={style.girl}>
+              <img src="" alt="" />
+            </div>
+            <p class={style.title}>Head Of PR</p>
+            <h3 class={style.name}>Lama Mohamed</h3>
+            <div class={style.social}>
+              <a href=""><img src={FB} alt="" /></a>
+              <a href=""><img src={IG} alt="" /></a>
+              <a href=""><img src={LI} alt="" /></a>
+            </div>
+          </div>
+          <div class={style.card}>
+            <div class={style.boy}>
+              <img src="" alt="" />
+            </div>
+            <p class={style.title}>Head Of Creative</p>
+            <h3 class={style.name}>Mina Bishoy</h3>
+            <div class={style.social}>
+              <a href=""><img src={FB} alt="" /></a>
+              <a href=""><img src={IG} alt="" /></a>
+              <a href=""><img src={LI} alt="" /></a>
+            </div>
+          </div>
+          <div class={style.card}>
+            <div class={style.girl}>
+              <img src="" alt="" />
+            </div>
+            <p class={style.title}>Head Of Telegram</p>
+            <h3 class={style.name}>Huda El Gendy</h3>
+            <div class={style.social}>
+              <a href=""><img src={FB} alt="" /></a>
+              <a href=""><img src={IG} alt="" /></a>
+              <a href=""><img src={LI} alt="" /></a>
+            </div>
+          </div>
+          <div class={style.card}>
+            <div class={style.boy}>
+              <img src="" alt="" />
+            </div>
+            <p class={style.title}>Head Of Web</p>
+            <h3 class={style.name}>Zeyad Emad</h3>
+            <div class={style.social}>
+              <a href=""><img src={FB} alt="" /></a>
+              <a href=""><img src={IG} alt="" /></a>
+              <a href=""><img src={LI} alt="" /></a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 

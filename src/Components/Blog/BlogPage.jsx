ed9b325bdfa7b8feb9740608af679c708d7d2d88
Yahia-153 <yahiametwally153@gmail.com>
2024-09-19 01:56:48 +0300

    Blog Page Update

--- src/Components/Blog/BlogPage.jsx
+++ src/Components/Blog/BlogPage.jsx
@@ -1,33 +1,57 @@
 import './BlogPage.css'
 import parse from 'html-react-parser';
+import circle from './Circle.png'
+const DotsPatern = () =>{
+  return(
+    <>
+    <div className="grid">
+      <img src={circle} alt="circle" />
+      <img src={circle} alt="circle" />
+      <img src={circle} alt="circle" />
+      <img src={circle} alt="circle" />
+      <img src={circle} alt="circle" />
+      <img src={circle} alt="circle" />
+      <img src={circle} alt="circle" />
+      <img src={circle} alt="circle" />
+      <img src={circle} alt="circle" />
+      <img src={circle} alt="circle" />
+      <img src={circle} alt="circle" />
+      <img src={circle} alt="circle" />
+      <img src={circle} alt="circle" />
+      <img src={circle} alt="circle" />
+      <img src={circle} alt="circle" />
+    </div>
+    </>
+  )
+}
 const BlogPage = () => {
 const events = [
     {
-        "id": 1,
         "brief": "Brief 3an el event",
         "date": "", 
         "image": ""
     },
     {
-        "id": 2,
         "brief": "Brief 3an el event",
         "date": "", 
         "image": ""
     },
     {
-        "id": 3,
         "brief": "Brief 3an el event",
         "date": "", 
         "image": ""
     },
     {
-        "id": 4,
         "brief": "Brief 3an el event",
         "date": "", 
         "image": ""
     },
     {
-        "id": 5,
+        "brief": "Brief 3an el event",
+        "date": "", 
+        "image": ""
+    },
+    {
         "brief": "Brief 3an el event",
         "date": "", 
         "image": ""
@@ -35,21 +59,24 @@ const events = [
     
 ]
 var test = "" ;
-events.forEach((event) =>{
+events.forEach((event , i) =>{
     var eventHTML = `
     <div className="event">
-          <div className="text">
-            <div className="brief">
-            <h2>${event.brief}</h2>
-            </div>
-            <div className="date">
-              <h2>date: ${event.date}</h2>
-            </div>
-          </div>
-          <div className="image">
-          <!--<img src="${event.image}" />-->
-          </div>
+      <div className='number'>
+        <p> ${i + 1} </p>
+      </div>
+      <div className="text">
+        <div className="brief">
+        <h2>${event.brief}</h2>
         </div>
+        <div className="date">
+          <h2>date: ${event.date}</h2>
+        </div>
+      </div>
+      <div className="image">
+        <!--<img src="${event.image}" />-->
+      </div>
+    </div>
     `
     test += eventHTML ;
   })
@@ -66,12 +93,17 @@ events.forEach((event) =>{
       <div className="scroll">
         <h1>Blog</h1>
         <i class="fas fa-arrow-down"></i>
-
+        <div className='dots top'>
+        <DotsPatern />
+        </div>
       </div>
       <section className="main" id='events'>
         {parse(test)}
       </section>
     <div className="scroll">
+    <div className='dots bottom'>
+      <DotsPatern />
+    </div>
     <i class="fas fa-arrow-up click" onClick={scrollToTop}></i>
     <h1>Go to top</h1>
     </div>

2bf128d842cfb852485125e8fe1fa54ee6123c73
janamohssen <165676621+janamohssen@users.noreply.github.com>
2024-09-20 00:44:25 +0300

    Update Hero.jsx

--- src/Components/OurStory/Hero/Hero.jsx
+++ src/Components/OurStory/Hero/Hero.jsx
@@ -1,8 +1,20 @@
-import './Hero.module.css'
+import styles from './Hero.module.css';
 
 const OurStoryHero = () => {
   return (
-    <div>OurStoryHero</div>
+    <>
+    <div className={styles.headerSection}>
+      <h1 className={styles.title}>We are a non-profit</h1>
+      <h1 className={styles.title}>organization</h1>
+      <h1 className={styles.title}>established in 2020</h1>
+      <p className={styles.subTitle}>lead by Youth for Youth</p>
+      <div className={styles.imageContainer}>
+        <img src="k" alt="team" className={styles.image} />
+      </div>
+  </div>
+  <hr />
+  </>
+
   )
 }
 

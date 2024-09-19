5e36134a3cfe688cc0f7bc748e1d89fcbac7c925
janamohssen <165676621+janamohssen@users.noreply.github.com>
2024-09-20 00:45:52 +0300

    Update OurMission.jsx

--- src/Components/OurStory/OurMission/OurMission.jsx
+++ src/Components/OurStory/OurMission/OurMission.jsx
@@ -1,8 +1,22 @@
-import './OurMission.module.css'
+import styles from './OurMission.module.css';
 
 const OurMission = () => {
   return (
-    <div>OurMission</div>
+    <div className={styles.missionSection}>
+    <h2 className={styles.missionTitle}>
+      <span className={styles.icon}>🤝</span> OUR MISSION
+    </h2>
+    <p className={styles.missionText}>
+      To empower and equip youth in high school and university level with the <br />necessary
+      skills, knowledge, and opportunities to succeed in their <br /> academic and professional
+      pursuits.
+    </p>
+    <p className={styles.missionText}>
+      We aim to provide a comprehensive <br /> set of resources, including training programs, <br />
+      online courses, and standardized test <br /> preparation materials that enable students
+      to excel in fields such as STEM.
+    </p>
+  </div>
   )
 }
 

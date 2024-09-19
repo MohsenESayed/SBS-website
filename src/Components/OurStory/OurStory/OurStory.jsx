3744dfeecb172d2a0b699a65ead175933aec0b1b
janamohssen <165676621+janamohssen@users.noreply.github.com>
2024-09-20 00:47:09 +0300

    Update OurStory.jsx

--- src/Components/OurStory/OurStory/OurStory.jsx
+++ src/Components/OurStory/OurStory/OurStory.jsx
@@ -1,8 +1,40 @@
-import './OurStory.module.css'
+import styles from './OurStory.module.css'
 
 const OurStory = () => {
   return (
-    <div>OurStory</div>
+    <div className={styles.OurStoryContainer}>
+      <h2 className={styles.title}>Our Story</h2>
+      <div className={styles.OurStory}>
+
+        <div className={styles.left}>
+          <div className={styles.event}>
+            <div className={styles.year}>2020</div>
+            <div className={styles.description}>..........</div>
+          </div>
+          <div className={styles.event}>
+            <div className={styles.year}>2022</div>
+            <div className={styles.description}>.........</div>
+          </div>
+          <div className={styles.event}>
+            <div className={styles.year}>2024</div>
+            <div className={styles.description}>.......</div>
+          </div>
+        </div>
+
+        <div className={styles.middleLine}></div>
+
+        <div className={styles.right}>
+          <div className={styles.event}>
+            <div className={styles.year}>2021</div>
+            <div className={styles.description}>.....</div>
+          </div>
+          <div className={styles.event}>
+            <div className={styles.year}>2023</div>
+            <div className={styles.description}>.........</div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 

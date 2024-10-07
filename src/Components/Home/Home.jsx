91a7e9c215d97518c569f5b125126178a4affff3
Zeiad <zezoemad1621265@gmail.com>
2024-10-06 23:26:31 -0500

    first verision of home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Hero from './Hero/HeroSeciton.jsx';
 import Overview from './Overview/Overview.jsx';
+import Numbers from './Numbers/Numbers.jsx';
 import CoursesCategory from './CoursesCategory/CoursesCategory.jsx';
 import Faqs from './Faqs/Faqs.jsx';
 
@@ -8,6 +9,7 @@ const Home = () => {
     <>
       <Hero />
       <Overview />
+      <Numbers />
       <CoursesCategory />
       <Faqs />
     </>
